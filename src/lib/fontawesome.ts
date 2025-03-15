1a57a651a312c0f8e0030ec8657b8845190d06f5
davarezza <dapva2728@gmail.com>
2025-03-15 14:14:46 +0700

    feat: styling for about section

--- src/lib/fontawesome.ts
+++ src/lib/fontawesome.ts
@@ -1,5 +1,17 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faUser, faHome, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faJs, faPhp, faLaravel, faReact } from "@fortawesome/free-brands-svg-icons";
 
-library.add(faUser, faHome, faEnvelope, faGithub, faLinkedin);
+library.add(
+  faUser,
+  faHome,
+  faEnvelope,
+  faGithub,
+  faLinkedin,
+  faJs,
+  faPhp,
+  faLaravel,
+  faReact,
+);
+
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,3 +1,154 @@
+import { Card } from "@/components/Card";
+import { SectionHeader } from "@/components/SectionHeader";
+import StarIcon from "@/assets/icons/star.svg";
+import bookImage from "@/assets/images/book-cover.png";
+import Image from "next/image";
+import JavaScriptIcon from "@/assets/icons/square-js.svg";
+import HTMLIcon from "@/assets/icons/html5.svg";
+import CSSIcon from "@/assets/icons/css3.svg";
+import ReactIcon from "@/assets/icons/react.svg";
+import ChromeIcon from "@/assets/icons/chrome.svg";
+import GithubIcon from "@/assets/icons/github.svg";
+import { TechIcon } from "@/components/TechIcon";
+import mapImage from "@/assets/images/map.png";
+import smileEmoji from "@/assets/images/memoji-smile.png";
+import { CardHeader } from "@/components/CardHeader";
+import { ToolboxItems } from "@/components/ToolboxItems";
+
+const toolboxItems = [
+  {
+    title: "JavaScript",
+    iconType: JavaScriptIcon,
+  },
+  {
+    title: "HTML",
+    iconType: HTMLIcon,
+  },
+  {
+    title: "CSS",
+    iconType: CSSIcon,
+  },
+  {
+    title: "React",
+    iconType: ReactIcon,
+  },
+  {
+    title: "Chrome",
+    iconType: ChromeIcon,
+  },
+  {
+    title: "Github",
+    iconType: GithubIcon,
+  },
+];
+
+const hobbies = [
+  {
+    title: "Music",
+    emoji: "🎵",
+    left: "5%",
+    top: "5%",
+  },
+  {
+    title: "Gaming",
+    emoji: "🎮",
+    left: "50%",
+    top: "5%",
+  },
+  {
+    title: "Gym",
+    emoji: "🏋️‍♂️",
+    left: "35%",
+    top: "40%",
+  },
+  {
+    title: "Reading",
+    emoji: "📖",
+    left: "7%",
+    top: "35%",
+  },
+  {
+    title: "Crypto",
+    emoji: "💰",
+    left: "70%",
+    top: "45%",
+  },
+  {
+    title: "Management",
+    emoji: "💸",
+    left: "5%",
+    top: "65%",
+  },
+  {
+    title: "Exploring",
+    emoji: "🔍",
+    left: "45%",
+    top: "70%",
+  },
+];
+
 export const AboutSection = () => {
-  return <div>About Section</div>;
+  return (
+    <div className="py-20">
+      <div className="container">
+        <SectionHeader
+          eyebrow="About Me"
+          title="A Glimpse Into My World"
+          description="Learn more about who i am, what i do, and what inspires me."
+        />
+        <div className="mt-20 flex flex-col gap-8">
+          <Card className="h-[320px]">
+            <CardHeader
+              title="My Reads"
+              description="Explore the books shaping my perspectives."
+            />
+            <div className="w-40 mx-auto mt-8">
+              <Image src={bookImage} alt="Book Cover" />
+            </div>
+          </Card>
+          <Card className="h-[320px] p-0">
+            <CardHeader
+              title="My Tech Stack"
+              description="Explore the programming languages, frameworks, and tools I use to build scalable and maintainable applications."
+              className="px-6 pt-6"
+            />
+            <ToolboxItems items={toolboxItems} className="mt-6" />
+            <ToolboxItems
+              items={toolboxItems}
+              className="mt-6"
+              itemsWrapperClassName="-translate-x-1/2"
+            />
+          </Card>
+          <Card className="h-[320px] p-0 flex flex-col">
+            <CardHeader
+              title="Beyond The Code"
+              description="Explore my interests and hobbies beyond the digital world."
+              className="px-6 py-6"
+            />
+            <div className="relative flex-1">
+              {hobbies.map((hobby) => (
+                <div
+                  key={hobby.title}
+                  className="inline-flex gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute"
+                  style={{ 
+                    left: hobby.left,
+                    top: hobby.top
+                   }}
+                >
+                  <span className="font-semibold text-gray-950">
+                    {hobby.title}
+                  </span>
+                  <span>{hobby.emoji}</span>
+                </div>
+              ))}
+            </div>
+          </Card>
+          <Card>
+            <Image src={mapImage} alt="Map" />
+            <Image src={smileEmoji} alt="Smile Emoji" />
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
 };
